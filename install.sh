622a0b9f50afff045f54d938b98b87cfc8cb42f0
miaoyc <miaoyc1989@hotmail.com>
2022-07-30 23:59:09 +0800

    ====feat: add copy ccat and config my alias====

--- install.sh
+++ install.sh
@@ -5,11 +5,20 @@ version=`lsb_release -a`
 echo $version
 
 
+# 1.copy ccat
+cp bin/ccat /usr/local/bin/ccat
+
+# 2.copy alias
+cp config/my_alias ~/.my_alias
+echo "" >> ~/.bashrc
+echo ". ~/.my_alias" >> ~/.bashrc
 
 # copy yum
 
 
 # copy apt
+
+
 
 
 
